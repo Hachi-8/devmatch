22a8a9306535682b56695b967b6c9759d15f583c
Hachi-8 <tanaka20180701@gmail.com>
2025-12-30 20:18:52 +0900

    chore: rm node_modules and dist from git

--- dist/profiles/profiles.guard.d.ts
+++
@@ -1,5 +0,0 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { Observable } from 'rxjs';
-export declare class ProfilesGuard implements CanActivate {
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean>;
-}
